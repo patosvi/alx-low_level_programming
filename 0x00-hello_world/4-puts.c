8559e7a3739211504b217f1cd9a975ae98d993ed
Patroba omollo <patoomollo@gmail.com>
2022-09-08 15:25:20 -0700

    cooments

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	 /* this will write the string to stdout */
+
+	char Mystr[] = ("Programming is like building a multilingual puzzle");
+
+
+		puts(Mystr) ; /* prints out put to stdout */
+
+		return (0);
+
+
+}
