438b456a0b589ff8ccbf1f39441e6e83bbbd232a
Patroba omollo <patoomollo@gmail.com>
2022-09-09 14:38:39 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabetic in lower and upper case
+ *
+ * Return : Always (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'A'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
