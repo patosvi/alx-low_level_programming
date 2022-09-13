8a6f7f64fe908cd48ad3064658081a4b11cb1fda
Patroba omollo <patoomollo@gmail.com>
2022-09-12 18:43:28 -0700

    prints revese alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print the revese alphabet.
+ *
+ * Return value :Always 0 (success)
+ */
+int main(void)
+{
+int p;
+
+for (p = 122; p > 96; p--)
+{
+putchar(p);
+}
+putchar('\n');
+return (0);
+}
+
+
