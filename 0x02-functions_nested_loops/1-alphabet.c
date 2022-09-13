f09f213542ac2cd55585a510a3ad5e0346307543
Patroba omollo <patoomollo@gmail.com>
2022-09-13 08:53:10 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * void print_alphabet(void); - Prints alphabet in lowercs
+ * @arg: the  void print_alphabet(void); will be used.
+ *
+ * Return: Always 0 (success) for return value of main .
+ *
+ */
+void print_alphabet(void)/*this is the main function*/
+{
+int i;
+
+for (i = 97; i <= 122;  i++)
+
+{
+_putchar(i);
+
+}
+
+_putchar('\n');
+}
