2ad595d35738c7f36730b10e41f2d71dddb5122b
Patroba omollo <patoomollo@gmail.com>
2022-09-14 10:39:01 -0700

    hotfix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -28,10 +28,10 @@ if (n >= 0 && n <= 15)
 	_putchar(op + 48);
 	}
 	else if (op <= 99)
-	{
-	 _putchar(32);
-	_putchar((op / 10) + 48);
-	_putchar((op % 10) + 48);
+	}
+_putchar(32);
+_putchar((op / 10) + 48);
+_putchar((op % 10) + 48);
 	}
 	else
 	{
