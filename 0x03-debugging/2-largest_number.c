d0922109aa2c8852d3ab043c42b6aa5c0f2140e0
Patroba omollo <patoomollo@gmail.com>
2022-09-15 10:29:11 -0700

    hotfix

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,12 +12,12 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a >= b && b >= c)
+if (a > b && b > c)
 {
 
-largest = largest_number(a, b, c);
+largest = a;
 }
-else if (b >= a && a >= c)
+else if (b > a && a > c)
 {
 largest = b;
 }
@@ -26,5 +26,5 @@ else
 largest = c;
 }
 
-return (0);
+return (largest);
 }
