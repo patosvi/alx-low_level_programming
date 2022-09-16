b28de13b9ce00cf02091dbfcb2f972ee5103dd54
Patroba omollo <patoomollo@gmail.com>
2022-09-15 18:19:30 -0700

    hotfix

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
+int largest_number(int, int, int);
+int convert_day(int, int);
 
+void print_remaining_days(int, int, int);
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
