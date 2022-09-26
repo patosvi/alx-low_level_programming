fa50ec87863cffa1a5981dc2154919fd32b88e03
patroba <patoomollo@gmail.com>
2022-09-26 20:01:21 +0000

    mem

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H_
 #define _MAIN_H_
-int_putchar(char);
+int _putchar(char);
 void set_string(char **s, char *to);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
