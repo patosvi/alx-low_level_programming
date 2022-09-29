6719c90d0728ce0971507efc8fe2844513efa281
patroba <patoomollo@gmail.com>
2022-09-29 20:10:47 +0000

    putchar

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,6 @@
 #ifdef MAIN_H_
-#define MAIN_H_
-int _putchar(char c);
+#define _ MAIN_H_
+int _putchar(char);
 void _putchar(char);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
