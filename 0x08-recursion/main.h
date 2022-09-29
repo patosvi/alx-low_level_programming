39e0f44a65665c2ba7f88da127017a79e63ef3fb
patroba <patoomollo@gmail.com>
2022-09-29 21:05:42 +0000

    string

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,5 +6,6 @@ void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
-
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 #endif
